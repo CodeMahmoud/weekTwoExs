25ee41d9c64e7edfd012553e4b67611b50f7e935
Mak <mahmoudcode913@gmail.com>
2021-10-31 20:11:57 -0500

    Iterate Ex

---
+++ Tuesday/IterateThroughObj.js
@@ -0,0 +1,20 @@
+function printObject(obj) {
+    // your code here...
+
+    for ( let key in obj){
+
+        let value = obj[key]
+        console.log( key + '-' + value)
+    }
+  }
+  
+  let bootcamp = {
+    name: "App Academy",
+    color: "Red",
+    population: 120
+  };
+  
+  printObject(bootcamp); // prints
+  // name - App Academy
+  // color - Red
+  // population - 120
